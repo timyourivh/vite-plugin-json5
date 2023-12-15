b0817389f3d6bede4d0de40b4b8a0a6c3b5675a8
YageStudios <120231768+YageStudios@users.noreply.github.com>
2023-12-15 16:40:20 -0500

    Use dataToEsm for export
    The dataToEsm is the same function used for the base Vite JSON plugin and provides better compatibility

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import JSON5 from 'json5'
 import type { Plugin } from 'vite'
+import { dataToEsm } from "@rollup/pluginutils";
 const fileRegex = /\.(jsonc|json5)$/
 
 export default function json5Plugin (): Plugin {
@@ -8,9 +9,17 @@ export default function json5Plugin (): Plugin {
 
     async transform (code: string, id: string) {
       if (fileRegex.test(id)) {
-        return {
-          code: `const data = ${JSON.stringify(JSON5.parse(code))};\nexport default data;`,
-          map: { mappings: '' }
+        try {
+          const parsed = JSON5.parse(json);
+          return {
+            code: dataToEsm(parsed, {
+              preferConst: true,
+              namedExports: options.namedExports,
+            }),
+            map: { mappings: "" },
+          };
+        } catch (e) {
+          this.error(`Failed to parse JSON file.`);
         }
       }
       return null;
