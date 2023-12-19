e2fd5e821e9106a1528ddf6104092d2632297eac
Tim van Herwijnen <t.vanherwijnen@helmink.nl>
2023-12-19 15:57:53 +0100

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,55 +1,54 @@
 import JSON5 from 'json5'
-import type { Plugin } from 'vite'
+import type { Plugin, JsonOptions } from 'vite'
 import { dataToEsm } from '@rollup/pluginutils'
-const fileRegex = /\.(jsonc|json5)$/
 
-export interface Json5Options {
-  /**
-   * Generate a named export for every property of the JSON object
-   * @default true
-   */
-  namedExports?: boolean
-  /**
-   * Generate performant output as JSON.parse('stringified').
-   * Enabling this will disable namedExports.
-   * @default false
-   */
-  stringify?: boolean
-}
+// Custom json filter for vite
+const json5ExtRE = /\.(jsonc|json5)$/
 
-export default function json5Plugin (
-  options: Json5Options = {}
+export interface Json5Options extends JsonOptions {}
+
+export function json5Plugin (
+  options: Json5Options = {},
+  isBuild: boolean
 ): Plugin {
   return {
-    name: 'vite:parse-json5',
+    name: 'vite:json5',
 
-    async transform (code: string, id: string) {
-      if (fileRegex.test(id)) {
+    transform (json, id) {
+      if (!json5ExtRE.test(id)) return null
+
+      try {
+        // Parse the JSON5
+        const parsed = JSON5.parse(json)
+
         if (options.stringify === true) {
-          return {
-            // during build, parse then double-stringify to remove all
-            // unnecessary whitespaces to reduce bundle size.
-            code: `export default JSON.parse(${JSON.stringify(
-              JSON.stringify(JSON5.parse(code))
-            )})`,
-            map: { mappings: '' }
+          json = JSON.stringify(parsed)
+
+          if (isBuild) {
+            return {
+              // during build, parse then double-stringify to remove all
+              // unnecessary whitespaces to reduce bundle size.
+              code: `export default JSON.parse(${JSON.stringify(
+                JSON.stringify(JSON.parse(json))
+              )})`,
+              map: { mappings: '' }
+            }
+          } else {
+            return `export default JSON.parse(${JSON.stringify(json)})`
           }
         }
 
-        try {
-          const parsed = JSON5.parse(code)
-          return {
-            code: dataToEsm(parsed, {
-              preferConst: true,
-              namedExports: options.namedExports
-            }),
-            map: { mappings: '' }
-          }
-        } catch (e) {
-          this.error('Failed to parse JSON file.')
+        return {
+          code: dataToEsm(parsed, {
+            preferConst: true,
+            namedExports: options.namedExports
+          }),
+          map: { mappings: '' }
         }
+      } catch (e) {
+        const error = e as Error
+        this.error(error.message)
       }
-      return null
     }
   }
 }
